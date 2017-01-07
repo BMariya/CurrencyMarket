17ab0f8f355e4377cd63f805852b78eee507fe46
mari <bazh.m.p@gmail.com>
2017-01-08 00:08:13 +0300

    hello world

---
+++ android-app/app/src/main/java/com/present/market/AppAct.java
@@ -0,0 +1,13 @@
+package com.present.market;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class AppAct extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.core_activity);
+    }
+}
