bf59a43b2405e7edc94c1700648cdce08f9503d7
mari <bazh.m.p@gmail.com>
2017-01-09 19:21:43 +0300

    xml objects

---
+++ android-app/app/src/main/java/com/present/market/obj/Valute.java
@@ -0,0 +1,64 @@
+package com.present.market.obj;
+
+import com.present.market.core.base.AbsObj;
+import com.present.market.core.base.AppType;
+
+import org.simpleframework.xml.Element;
+import org.simpleframework.xml.Root;
+
+@Root(name = "Valute")
+public final class Valute extends AbsObj {
+    @Element(name = "CharCode")
+    private String mCharCode;
+    @Element(name = "Name")
+    private String mName;
+    @Element(name = "Nominal")
+    private int mNominal;
+    @Element(name = "Value")
+    private String mValue;
+
+    private Valute() {
+        super();
+    }
+    public Valute(String charCode, String name, int nominal, String value) {
+        this();
+        log().debug("charCode=%s,name=%s,nominal=%s,value=%s", charCode, name, nominal, value);
+        this.mCharCode = charCode;
+        this.mName = name;
+        this.mNominal = nominal;
+        this.mValue = value;
+    }
+
+    public String getCharCode() {
+        return this.mCharCode;
+    }
+
+    public String getName() {
+        return this.mName;
+    }
+
+    public AppType.AppAmount getNominal() {
+        return new AppType.AppAmount(this.mNominal);
+    }
+
+    public AppType.AppAmount getValue() {
+        log().todo("getValue.setValue? convert value type?");
+        return new AppType.AppAmount(this.mValue);
+    }
+
+    public AppType.AppAmount getRefValue(Valute refValute, AppType.AppAmount refAmount) {
+        log().debug("getRefValue.refValute=%s,refAmount=%s", refValute, refAmount);
+        AppType.AppAmount result = new AppType.AppAmount(
+                refAmount.value * (this.getValue().value / this.getNominal().value)
+                / (refValute.getValue().value / refValute.getNominal().value));
+        log().debug("getRefValue.result=%s", result);
+        return result;
+    }
+
+    public final boolean equals(Valute value) {
+        return this.getCharCode().equals(value.getCharCode()) &&
+                this.getName().equals(value.getName()) &&
+                this.getNominal() == value.getNominal() &&
+                this.getValue().equals(value.getValue());
+    }
+}
---
+++ android-app/app/src/main/java/com/present/market/obj/Valutes.java
@@ -0,0 +1,33 @@
+package com.present.market.obj;
+
+import com.present.market.core.base.AbsObj;
+import com.present.market.core.base.AppType;
+
+import org.simpleframework.xml.Attribute;
+import org.simpleframework.xml.ElementList;
+import org.simpleframework.xml.Root;
+
+import java.util.List;
+
+@Root(name = "ValCurs")
+public final class Valutes extends AbsObj {
+    @Attribute(name = "name")
+    private String mName;
+    @Attribute(name = "Date")
+    private String mDate;
+    @ElementList(inline = true)
+    private List<Valute> mValuteList;
+
+    public String getName() {
+        return this.mName;
+    }
+
+    public AppType.AppDate getDate() {
+        return new AppType.AppDate(this.mDate);
+    }
+
+    public List<Valute> getValuteList() {
+        log().todo("getValuteList.AppType.AppList List? convert value type?");
+        return this.mValuteList;
+    }
+}
