2b5b521e6ad87bb31cbed18c8d047b49491bcfa7
mari <bazh.m.p@gmail.com>
2017-01-09 19:55:12 +0300

    activity

--- android-app/app/src/main/java/com/present/market/AppAct.java
+++ android-app/app/src/main/java/com/present/market/AppAct.java
@@ -1,13 +1,63 @@
 package com.present.market;
 
-import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
+import com.present.market.core.AbsAppAct;
+import com.present.market.core.base.AppAction;
+import com.present.market.core.base.AppEx;
+import com.present.market.core.base.AppCallback;
+import com.present.market.core.base.AppType;
+import com.present.market.core.ui.TextChangeAction;
+import com.present.market.obj.Valute;
+import com.present.market.ui.InitFrame;
+import com.present.market.ui.MainFrame;
 
-public class AppAct extends AppCompatActivity {
+public final class AppAct extends AbsAppAct implements AppCallback<AppStore> {
+    private InitFrame mInitFrame;
+    private MainFrame mMainFrame;
+    @Override
+    protected void onInit() {
+        this.mMainFrame = new MainFrame(getFrameContentView());
+        showFrame(this.mMainFrame);
+        long appInitTimeMs = app().getAppInitDurationInMs();
+        if (appInitTimeMs > 0) {
+            this.mInitFrame = new InitFrame(getFrameContentView());
+            showFrame(this.mInitFrame, appInitTimeMs);
+        }
+        app().store().register(this);
+    }
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.core_activity);
+    public void onOut() {
+        app().store().unregister(this);
+    }
+
+    @Override
+    protected final App app() {
+        return (App) super.app();
+    }
+
+    @Override
+    public void onResult(AppStore appStore) {
+        log().debug("updateData");
+        this.mMainFrame.show(appStore.getTitle(), appStore.getDate(), appStore.getRefValute(),
+                appStore.getRefAmount(), appStore.getValuteList(),
+                new TextChangeAction() {
+
+                    @Override
+                    public void onResult(String refAmount) {
+                        app().store().setRefAmount(new AppType.AppAmount(refAmount));
+                    }
+                }, new AppAction<Valute>() {
+
+                    @Override
+                    public void onResult(Valute valute) {
+                        app().store().setRefValute(valute);
+                    }
+                });
+    }
+
+    @Override
+    public void onError(AppEx appEx) {
+        log().debug("updateError");
+        showMessage(appEx.getMessage());
     }
 }
---
+++ android-app/app/src/main/java/com/present/market/core/AbsAppAct.java
@@ -0,0 +1,64 @@
+package com.present.market.core;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.ViewGroup;
+import android.widget.Toast;
+
+import com.present.market.R;
+import com.present.market.core.base.AppLog;
+import com.present.market.core.base.AppType;
+import com.present.market.core.ui.AbsFrame;
+
+public abstract class AbsAppAct extends AppCompatActivity {
+    private ViewGroup mFrameContentView;
+    @Override
+    protected final void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.core_activity);
+        this.mFrameContentView = (ViewGroup) findViewById(R.id.core_activity__frame_content);
+        log().info_init();
+        this.onInit();
+    }
+
+    protected abstract void onInit();
+
+    public final void onDestroy() {
+        log().info_out();
+        this.onOut();
+        super.onDestroy();
+    }
+    protected abstract void onOut();
+
+    protected final ViewGroup getFrameContentView() {
+        return this.mFrameContentView;
+    }
+
+    public final void showFrame(AbsFrame frame) {
+        log().debug("showFrame");
+        this.mFrameContentView.addView(frame.getView());
+    }
+
+    public final void showFrame(AbsFrame frame, long durationInMs) {
+        log().debug("showFrame.durationInMs=%s", durationInMs);
+        this.showFrame(frame);
+        frame.show(durationInMs);
+    }
+
+    public final void showMessage(String message) {
+        log().debug("showMessage.message=%s", message);
+        Toast.makeText(this, message, Toast.LENGTH_LONG).show();
+    }
+
+    private AppLog mLog;
+    protected final AppLog log() {
+        if (AppType.OBJ_IS_NULL(this.mLog)) this.mLog = new AppLog(this);
+        return this.mLog;
+    }
+
+    private AbsApp mApp;
+    protected AbsApp app() {
+        if (AppType.OBJ_IS_NULL(this.mApp)) this.mApp = (AbsApp) getApplication();
+        return this.mApp;
+    }
+}
