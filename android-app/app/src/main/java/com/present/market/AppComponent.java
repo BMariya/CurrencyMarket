b4c34bb1533f877286d6772ff2beedbf0a06a3e9
Mary <mary.waft@gmail.com>
2018-02-12 14:08:22 +0300

    dagger-2 test custom scope

--- android-app/app/src/main/java/com/present/market/AppComponent.java
+++ android-app/app/src/main/java/com/present/market/AppComponent.java
@@ -1,5 +1,11 @@
 package com.present.market;
 
+import com.present.market.core.AbsApp;
+import com.present.market.core.store.LocalSource;
+import com.present.market.core.store.RemoteSource;
+import com.present.market.obj.Valute;
+import com.present.market.obj.Valutes;
+
 import javax.inject.Singleton;
 
 import dagger.Component;
@@ -7,5 +13,8 @@ import dagger.Component;
 @Singleton
 @Component(modules = {AppModule.class})
 public interface AppComponent {
-    void inject(AppAct appAct);
+    Valute valuteRuble();
+    LocalSource<Valutes> localSource();
+    RemoteSource<Valutes> remoteSource();
+//    void inject(AppAct appAct);
 }
